169d60d09b01cc6fe6af46ec932ff4b9977a7209
ThatSiya <zwane.bonga@gmail.com>
2018-09-13 16:18:42 +0200

    updated plantation controller

--- farmLogin/Controllers/PlantationController.cs
+++ farmLogin/Controllers/PlantationController.cs
@@ -19,7 +19,9 @@ namespace farmLogin.Controllers
     public class PlantationController : Controller
     {
         private FarmDbContext db = new FarmDbContext();
+        public static int routeId;
 
+        #region CRUD and ActionPage
         public ActionResult ActionPage()
         {
             return View();
@@ -29,11 +31,13 @@ namespace farmLogin.Controllers
 
         public ActionResult Index()
         {
+            var plantation = db.Plantations.Include(p => p.CropCycle).Include(p => p.CropType).Include(p => p.Field).Include(p => p.SiloHarvests).Where(p => p.PlantationStatus == "Planned");
             return View();
         }
 
         public ActionResult Plantations()
         {
+            var plantation = db.Plantations.Include(p => p.CropCycle).Include(p => p.CropType).Include(p => p.Field).Include(p => p.SiloHarvests).Where(p => p.PlantationStatus == "Confirmed");
             return View();
         }
 
@@ -100,139 +104,6 @@ namespace farmLogin.Controllers
             ViewBag.RefugeUnit = new SelectList(db.Units, "UnitID", "UnitDescr", plantation.RefugeUnit);
             ViewBag.YieldUnit = new SelectList(db.Units, "UnitID", "UnitDescr", plantation.YieldUnit);
             return View(plantation);
-        }
-
-
-
-
-
-        public ActionResult Set(int? id)
-        {
-            if (ModelState.IsValid)
-            {
-                if (id == null)
-                {
-                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-                }
-                Plantation plantation = db.Plantations.Find(id);
-
-                plantation.FieldStageID = 5;
-                plantation.JavaScriptToRun = "mySuccess()";
-                if (plantation == null)
-                {
-                    return HttpNotFound();
-                }
-
-                try
-                {
-                    db.SaveChanges();
-                    return RedirectToAction("Plantations", plantation);
-                }
-                catch (System.Data.Entity.Validation.DbEntityValidationException dbEx)
-                {
-                    Exception raise = dbEx;
-                    foreach (var validationErrors in dbEx.EntityValidationErrors)
-                    {
-                        foreach (var validationError in validationErrors.ValidationErrors)
-                        {
-                            string message = string.Format("{0}:{1}",
-                                validationErrors.Entry.Entity.ToString(),
-                                validationError.ErrorMessage);
-                            // raise a new exception nesting
-                            // the current instance as InnerException
-                            raise = new InvalidOperationException(message, raise);
-                        }
-                    }
-                    throw raise;
-                }
-
-            }
-            return View();
-
-        }
-
-        ///////////////////////////Confirm Plantation//////////////////////////////
-        //GET
-        public ActionResult Confirm(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Plantation plantation = db.Plantations.Find(id);
-            if(id == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.CropCycleID = new SelectList(db.CropCycles, "CropCycleID", "CropCycleDescr", plantation.CropCycleID);
-            ViewBag.CropTypeID = new SelectList(db.CropTypes, "CropTypeID", "CropTypeDescr", plantation.CropTypeID);
-            ViewBag.FieldID = new SelectList(db.Fields, "FieldID", "FieldName", plantation.FieldID);
-            ViewBag.FieldStageID = new SelectList(db.FieldStages, "FieldStageID", "FieldStageDescr", plantation.FieldStageID);
-            ViewBag.RefugeUnit = new SelectList(db.Units, "UnitID", "UnitDescr", plantation.RefugeUnit);
-            ViewBag.YieldUnit = new SelectList(db.Units, "UnitID", "UnitDescr", plantation.YieldUnit);
-            return View(plantation);
-        }
-        //Post
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Confirm([Bind(Include = "PlantationID,FieldID,CropTypeID,CropCycleID,FieldStageID,DatePlanted,RefugeSeedAmntUsed,RefugeUnit,RefugeAreaHectares,ExpectedYieldQnty,YieldUnit,DateHarvested,PlantationStatus")] Plantation plantation)
-        {
-            if (ModelState.IsValid)
-            {
-                var myPlantation = db.Plantations.Where(p => p.PlantationID == plantation.PlantationID).SingleOrDefault();
-                using (FarmDbContext dc = new FarmDbContext())
-                {
-                    //Add plantation
-                    //plantation.FieldStageID = 1; //Stage 1 - Planted
-                    //plantation.PlantationStatus = "Confirmed";
-                    //dc.Plantations.Add(plantation);
-                    //dc.SaveChanges();
-                    //Update the field status
-
-                    //return RedirectToAction("Index");
-                    myPlantation.PlantationID = plantation.PlantationID;
-                    myPlantation.FieldID = plantation.FieldID;
-                    myPlantation.CropTypeID = plantation.CropTypeID;
-                    myPlantation.CropCycleID = plantation.CropCycleID;
-                    myPlantation.FieldStageID = 1; //Stage 1 -Planned
-                    myPlantation.DatePlanted = plantation.DatePlanted;
-                    myPlantation.RefugeSeedAmntUsed = plantation.RefugeSeedAmntUsed;
-                    myPlantation.RefugeUnit = plantation.RefugeUnit;
-                    myPlantation.RefugeAreaHectares = plantation.RefugeAreaHectares;
-                    myPlantation.ExpectedYieldQnty = plantation.ExpectedYieldQnty;
-                    myPlantation.YieldUnit = plantation.YieldUnit;
-                    myPlantation.DateHarvested = plantation.DateHarvested;
-                    myPlantation.PlantationStatus = "Confirmed";
-
-                    db.SaveChanges();
-                    plantation.JavaScriptToRun = "mySuccess()";
-                    TempData["yay"] = plantation;
-
-                    ViewBag.CropCycleID = new SelectList(db.CropCycles, "CropCycleID", "CropCycleDescr", plantation.CropCycleID);
-                    ViewBag.CropTypeID = new SelectList(db.CropTypes, "CropTypeID", "CropTypeDescr", plantation.CropTypeID);
-                    ViewBag.FieldID = new SelectList(db.Fields, "FieldID", "FieldName", plantation.FieldID);
-                    ViewBag.FieldStageID = new SelectList(db.FieldStages, "FieldStageID", "FieldStageDescr", plantation.FieldStageID);
-                    ViewBag.RefugeUnit = new SelectList(db.Units, "UnitID", "UnitDescr", plantation.RefugeUnit);
-                    ViewBag.YieldUnit = new SelectList(db.Units, "UnitID", "UnitDescr", plantation.YieldUnit);
-                    return View(plantation);
-                }
-
-            }
-
-                int Id = plantation.FieldID;
-                var field = db.Fields.Find(Id);
-                field.FieldStatusID = 2; //In-Use
-                db.Entry(field).State = EntityState.Modified;
-                db.SaveChanges();
-
-            ViewBag.CropCycleID = new SelectList(db.CropCycles, "CropCycleID", "CropCycleDescr", plantation.CropCycleID);
-            ViewBag.CropTypeID = new SelectList(db.CropTypes, "CropTypeID", "CropTypeDescr", plantation.CropTypeID);
-            ViewBag.FieldID = new SelectList(db.Fields, "FieldID", "FieldName", plantation.FieldID);
-            ViewBag.FieldStageID = new SelectList(db.FieldStages, "FieldStageID", "FieldStageDescr", plantation.FieldStageID);
-            ViewBag.RefugeUnit = new SelectList(db.Units, "UnitID", "UnitDescr", plantation.RefugeUnit);
-            ViewBag.YieldUnit = new SelectList(db.Units, "UnitID", "UnitDescr", plantation.YieldUnit);
-            return RedirectToAction("Index");
-            //return View(plantation);
         }
 
         // GET: Plantation/Edit/5
@@ -331,6 +202,196 @@ namespace farmLogin.Controllers
             db.SaveChanges();
             return RedirectToAction("Index");
         }
+
+        #endregion
+
+
+        public ActionResult Set(int? id)
+        {
+            if (ModelState.IsValid)
+            {
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                Plantation plantation = db.Plantations.Find(id);
+
+                plantation.FieldStageID = 5;
+                plantation.JavaScriptToRun = "mySuccess()";
+                if (plantation == null)
+                {
+                    return HttpNotFound();
+                }
+
+                try
+                {
+                    db.SaveChanges();
+                    return RedirectToAction("Plantations", plantation);
+                }
+                catch (System.Data.Entity.Validation.DbEntityValidationException dbEx)
+                {
+                    Exception raise = dbEx;
+                    foreach (var validationErrors in dbEx.EntityValidationErrors)
+                    {
+                        foreach (var validationError in validationErrors.ValidationErrors)
+                        {
+                            string message = string.Format("{0}:{1}",
+                                validationErrors.Entry.Entity.ToString(),
+                                validationError.ErrorMessage);
+                            // raise a new exception nesting
+                            // the current instance as InnerException
+                            raise = new InvalidOperationException(message, raise);
+                        }
+                    }
+                    throw raise;
+                }
+
+            }
+            return View();
+
+        }
+
+        #region Confirm Plantation
+        //GET
+        public ActionResult Confirm(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Plantation plantation = db.Plantations.Find(id);
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CropCycleID = new SelectList(db.CropCycles, "CropCycleID", "CropCycleDescr", plantation.CropCycleID);
+            ViewBag.CropTypeID = new SelectList(db.CropTypes, "CropTypeID", "CropTypeDescr", plantation.CropTypeID);
+            ViewBag.FieldID = new SelectList(db.Fields, "FieldID", "FieldName", plantation.FieldID);
+            ViewBag.FieldStageID = new SelectList(db.FieldStages, "FieldStageID", "FieldStageDescr", plantation.FieldStageID);
+            ViewBag.RefugeUnit = new SelectList(db.Units, "UnitID", "UnitDescr", plantation.RefugeUnit);
+            ViewBag.YieldUnit = new SelectList(db.Units, "UnitID", "UnitDescr", plantation.YieldUnit);
+            return View(plantation);
+        }
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Confirm([Bind(Include = "PlantationID,FieldID,CropTypeID,CropCycleID,FieldStageID,DatePlanted,RefugeSeedAmntUsed,RefugeUnit,RefugeAreaHectares,ExpectedYieldQnty,YieldUnit,DateHarvested,PlantationStatus")] Plantation plantation)
+        {
+            if (ModelState.IsValid)
+            {
+                var myPlantation = db.Plantations.Where(p => p.PlantationID == plantation.PlantationID).SingleOrDefault();
+                using (FarmDbContext dc = new FarmDbContext())
+                {
+                    //Add plantation
+                    //plantation.FieldStageID = 1; //Stage 1 - Planted
+                    //plantation.PlantationStatus = "Confirmed";
+                    //dc.Plantations.Add(plantation);
+                    //dc.SaveChanges();
+                    //Update the field status
+
+                    //return RedirectToAction("Index");
+                    myPlantation.PlantationID = plantation.PlantationID;
+                    myPlantation.FieldID = plantation.FieldID;
+                    myPlantation.CropTypeID = plantation.CropTypeID;
+                    myPlantation.CropCycleID = plantation.CropCycleID;
+                    myPlantation.FieldStageID = 1; //Stage 1 -Planned
+                    myPlantation.DatePlanted = plantation.DatePlanted;
+                    myPlantation.RefugeSeedAmntUsed = plantation.RefugeSeedAmntUsed;
+                    myPlantation.RefugeUnit = plantation.RefugeUnit;
+                    myPlantation.RefugeAreaHectares = plantation.RefugeAreaHectares;
+                    myPlantation.ExpectedYieldQnty = plantation.ExpectedYieldQnty;
+                    myPlantation.YieldUnit = plantation.YieldUnit;
+                    myPlantation.DateHarvested = plantation.DateHarvested;
+                    myPlantation.PlantationStatus = "Confirmed";
+
+                    db.SaveChanges();
+                    plantation.JavaScriptToRun = "mySuccess()";
+                    TempData["yay"] = plantation;
+
+                    ViewBag.CropCycleID = new SelectList(db.CropCycles, "CropCycleID", "CropCycleDescr", plantation.CropCycleID);
+                    ViewBag.CropTypeID = new SelectList(db.CropTypes, "CropTypeID", "CropTypeDescr", plantation.CropTypeID);
+                    ViewBag.FieldID = new SelectList(db.Fields, "FieldID", "FieldName", plantation.FieldID);
+                    ViewBag.FieldStageID = new SelectList(db.FieldStages, "FieldStageID", "FieldStageDescr", plantation.FieldStageID);
+                    ViewBag.RefugeUnit = new SelectList(db.Units, "UnitID", "UnitDescr", plantation.RefugeUnit);
+                    ViewBag.YieldUnit = new SelectList(db.Units, "UnitID", "UnitDescr", plantation.YieldUnit);
+                    return View(plantation);
+                }
+
+            }
+
+            int Id = plantation.FieldID;
+            var field = db.Fields.Find(Id);
+            field.FieldStatusID = 2; //In-Use
+            db.Entry(field).State = EntityState.Modified;
+            db.SaveChanges();
+
+            ViewBag.CropCycleID = new SelectList(db.CropCycles, "CropCycleID", "CropCycleDescr", plantation.CropCycleID);
+            ViewBag.CropTypeID = new SelectList(db.CropTypes, "CropTypeID", "CropTypeDescr", plantation.CropTypeID);
+            ViewBag.FieldID = new SelectList(db.Fields, "FieldID", "FieldName", plantation.FieldID);
+            ViewBag.FieldStageID = new SelectList(db.FieldStages, "FieldStageID", "FieldStageDescr", plantation.FieldStageID);
+            ViewBag.RefugeUnit = new SelectList(db.Units, "UnitID", "UnitDescr", plantation.RefugeUnit);
+            ViewBag.YieldUnit = new SelectList(db.Units, "UnitID", "UnitDescr", plantation.YieldUnit);
+            return RedirectToAction("Index");
+            //return View(plantation);
+        }
+
+        #endregion
+
+
+        #region Add Harvest to Silo
+        //GET
+        public ActionResult Harvest(int? id) //plantationID
+        {
+            if (id != null)
+            {
+                TempData["plantationId"] = id;
+            }
+            using (FarmDbContext dc = new FarmDbContext())
+            {
+                //var Id = dc.Plantations.Find(id);
+                routeId = Convert.ToInt32(id); //pass this Id at the end to get the field
+            }
+            ViewBag.PlantationID = new SelectList(db.Plantations, "PlantationID", "PlantationStatus");
+            ViewBag.SiloID = new SelectList(db.Silos, "SiloID", "SiloDescr");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Harvest([Bind(Include = "SiloHarvestID,SiloHarvestStoreStartDate,SiloHarvestStoreEndDate,SiloHarvestTonnesStored,SiloID,PlantationID")] SiloHarvest siloHarvest)
+        {
+
+            if (ModelState.IsValid)
+            {
+                int plantationID = (int)TempData["plantationId"];
+                siloHarvest.PlantationID = plantationID;
+                db.SiloHarvests.Add(siloHarvest);
+                db.SaveChanges();
+
+                //Calculate difference between ExpectedYield and SiloHarvestTonnes stored
+                using (FarmDbContext dc = new FarmDbContext())
+                {
+                    var plantation = dc.Plantations.Find(plantationID);
+                    var expYield = plantation.ExpectedYieldQnty;
+                    var siloHarvestTStored = siloHarvest.SiloHarvestTonnesStored;
+                    var yieldDiff = expYield - siloHarvestTStored;
+                    //float yieldDiff = plantation.ExpectedYieldQnty - siloHarvest.SiloHarvestTonnesStored;
+                    //if (yieldDiff > 0)
+                    //{
+                    //    return RedirectToAction("Reason");
+                    //    //return Reason(plantationID);
+                    //}
+                }
+
+                return RedirectToAction("Index", "Plantation");
+            }
+
+            ViewBag.PlantationID = new SelectList(db.Plantations, "PlantationID", "PlantationStatus", siloHarvest.PlantationID);
+            ViewBag.SiloID = new SelectList(db.Silos, "SiloID", "SiloDescr", siloHarvest.SiloID);
+            return View(siloHarvest);
+        }
+
+        #endregion
 
         protected override void Dispose(bool disposing)
         {
@@ -455,6 +516,8 @@ namespace farmLogin.Controllers
             return new JsonResult { Data = new { status = status } };
         }
 
+
+        #region Redirect Functions
         [HttpPost]
         public ActionResult RedirectToEdit(int plantationID)
         {
@@ -468,6 +531,7 @@ namespace farmLogin.Controllers
             string myUrl = "/Plantation/Set/" + plantationID;
             return Json(new { Url = myUrl });
         }
+
         [HttpPost]
         public ActionResult RedirectToConfirm(int plantationID)
         {
@@ -475,8 +539,14 @@ namespace farmLogin.Controllers
             return Json(new { Url = myUrl });
         }
 
+        [HttpPost]
+        public ActionResult RedirectToHarvest(int? plantationID)
+        {
+            string url = "/Plantation/Harvest/" + plantationID;
+            return Json(new { Url = url });
+        }
 
-
+        #endregion
 
 
     }
